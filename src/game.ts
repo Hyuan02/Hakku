a4961dd466298194aff57bf5c7f7908a47b39295
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2020-10-05 21:17:13 -0300

    initial template from oficial photonstorm

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,40 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
+
+export default class Fog extends Phaser.Scene{
+    constructor(){
+        super('fog')
     }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
+    preload(){
+        
+        this.load.image('tiles', 'assets/Maps/roguelikeDungeon_transparent.png');
         this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.tilemapTiledJSON('map', 'assets/Maps/Cave.json');
     }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+    create(){
+        const map = this.make.tilemap({key: 'map'});
+        const tileset = map.addTilesetImage('roguelikeDungeon_transparent','tiles');
+        const floor = map.createStaticLayer('FLOOR', tileset, 0,0);
+        const walls = map.createStaticLayer('WALLS', tileset, 0,0);
+        const walkable = map.createStaticLayer('WALKABLE', tileset,0,0);
+        // this.add.image(400, 300, 'libs');
     }
+
+    
 }
 
 const config = {
     type: Phaser.AUTO,
     backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+    width: 1920,
+    height: 1080,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: Fog
 };
 
 const game = new Phaser.Game(config);
